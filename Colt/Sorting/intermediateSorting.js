f3efe16475f24806fd7a61e25be8bad0f2c9bbb8
Steve Johnson <steve21johnson@gmail.com>
2020-03-28 12:51:41 -0700

    Radix Sort

--- Colt/Sorting/intermediateSorting.js
+++ Colt/Sorting/intermediateSorting.js
@@ -54,4 +54,72 @@ const quickSort = (array, start = 0, end = array.length - 1) => {
 
 }
 
-module.exports = {merge, mergeSort, pivot, quickSort}
+function radixSortA(array) {
+  let maxLength = 0
+  for (const number of array) {
+    if (String(number).length > maxLength) maxLength = String(number).length
+  }
+  let a = [array]
+  let b = []
+  for (let place = 0; place < maxLength; place++) {
+    let bucketIndex = 0
+
+    while (bucketIndex < 10) {
+      const number = a[bucketIndex] && a[bucketIndex].shift()
+      if (number === undefined) {
+        bucketIndex++
+        continue
+      }
+      const digit = String(number)[String(number).length - 1 - place]
+      //push into numbers
+      if (digit) {
+        if (b[digit]) b[digit].push(number)
+        else b[digit] = [number]
+      } else {
+        if (b[0]) b[0].push(number)
+        else b[0] = [number]
+      }
+
+    }
+    a = b
+    b = []
+
+  }
+  //ES2019
+  return a.flat()
+
+  // return flatten(a)
+}
+
+function flatten(array) {
+  if (array.length <= 1) return array
+  let result = []
+  array.forEach(element => {
+    if (Array.isArray(element)) result = result.concat(flatten(element))
+    else result.push(element)
+  })
+  return result
+}
+
+function radixSortB(array) {
+  let maxLength = 0
+  for (const number of array) {
+    maxLength = Math.max(String(number).length, maxLength)
+  }
+
+  for (let place = 0; place < maxLength; place++) {
+    let buckets = Array.from({length: 10}, () => [])
+
+    for (let i = 0; i < array.length; i++) {
+      const number = array[i]
+      let digit = String(number)[String(number).length - 1 - place]
+      if (!digit) digit = 0
+      buckets[digit].push(number)
+    }
+    array = [].concat(...buckets)
+  }
+  return array
+
+}
+
+module.exports = {merge, mergeSort, pivot, quickSort, radixSort: radixSortB}
--- Colt/Sorting/intermediateSorting.test.js
+++ Colt/Sorting/intermediateSorting.test.js
@@ -1,4 +1,4 @@
-const {merge, mergeSort, pivot, quickSort} = require("./intermediateSorting")
+const {merge, mergeSort, pivot, quickSort, radixSort} = require("./intermediateSorting")
 
 test("merge", () => {
   expect(merge([1, 3], [2, 4])).toStrictEqual([1, 2, 3, 4])
@@ -22,4 +22,9 @@ test("pivot", () => {
 test("quickSort", () => {
   const array = [5,2,1,8,4,7,6,3]
   expect(quickSort(array)).toStrictEqual([1,2,3,4,5,6,7,8])
+})
+
+test("radixSort", () => {
+  expect(radixSort([5,2,1,88,4,713,6,3])).toStrictEqual([1,2,3,4,5,6,88,713])
+  expect(radixSort([12,1004,913,8])).toStrictEqual([8,12,913,1004])
 })
